0522a06ee74a7d8f4c9b3c873e23c53a7634da59
acraig9266 <158104649+acraig9266@users.noreply.github.com>
2024-10-01 12:26:48 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,23 @@
 function permutationSort(a) {
-    
-    
-    return a;
+    permCount = 0;
+    while (isSorted(a) = false) {
+        a = newPerm(a, permCount);
+        permCount += 1;
+    }
+    return permCount;
+}
+
+function isSorted(array){
+    for(i = 0; i < array.length; i++){
+        if (array[i] > array[i + 1]){
+            return false;
+        }
+    }
+    return true;
+}
+
+function newPerm(array, n){
+    for(i = 0; i < array.length; i++){
+        
+    }
 }

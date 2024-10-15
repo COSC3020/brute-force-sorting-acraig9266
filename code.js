98c33600845135071c5f4da87f45150d3f8e7dd0
acraig9266 <158104649+acraig9266@users.noreply.github.com>
2024-10-15 14:07:44 -0600

    Update code.js

--- code.js
+++ code.js
@@ -3,9 +3,12 @@ function swap(arr, i, j) {
 }
 
 function permutationSort(arr) {
-    perms = 1;
+    if (isSorted(arr)) {
+        return 1;
+    }
     const n = arr.length;
     const c = new Array(n).fill(0); // Control array initialized to 0
+    perms = 1;
     let i = 0;
     while (i < n) {
         if (c[i] < i) {
@@ -13,9 +16,9 @@ function permutationSort(arr) {
             c[i]++;
             i = 1; // Reset index
             perms++;
-            if (isSorted(arr)) { // Moved count and check into if statement so only increments and checks when new permutation is actually made
-            return perms;
-        }
+            if (isSorted(arr)) { // Moved count and check into if statement so only increments and checks when new perm made
+                return perms;
+            }
         } else {
             c[i] = 0;
             i++; // Move to the next index
